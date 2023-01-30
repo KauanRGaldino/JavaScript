3177da3604f7b08c9a2a3d76f8f5cbe319cedf95
KAUAN RODRIGUES GALDINO <kauan.rgaldino@senacsp.edu.br>
2023-01-30 11:53:19 -0300

    JavaScript

---
+++ Operadores.js
@@ -0,0 +1,16 @@
+/*let a = 514
+let b = 21
+let c = 604
+
+console.log(a == b)
+*/
+
+let idade =40
+
+if (idade >= 16 && idade< 18){
+ console.log("o voto é opcional")
+} else if (idade < 16){
+    console.log("Voce não pode votar")
+} else {
+    console.log("o voto é obrigatorio")
+}
---
+++ script.js
@@ -0,0 +1,29 @@
+/*  let nome = "Risadinha" //STRING
+let idade = 19 // INT OU NUMBER
+let altura = 1.50 //FLOAT OU NUMBER
+let temGato = true // BOOLEAN
+
+console.log("Oi eu sou " +nome+ "e eu tenho " +idade+ " anos")
+
+console.log(`Oi eu sou ${nome} e eu tenho ${idade} anos`)
+
+//OBJETOS
+
+let pessoas = {
+    nome: 'Nanda',
+    idade: 38,
+    sexo: 'F',
+    cpf: '421.886.111-00',
+    altura: 1.80
+
+}
+
+console.log(pessoas.idade)
+
+*/
+
+let n1 =624
+let n2 =104
+
+let resultado = n1+n2
+console.log(resultado)
