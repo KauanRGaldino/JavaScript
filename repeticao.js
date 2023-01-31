32e6396e8083d021763a69e19a2204bf1ee06485
KAUAN RODRIGUES GALDINO <kauan.rgaldino@senacsp.edu.br>
2023-01-31 11:21:59 -0300

    Continuação das Atividades JavaScript

---
+++ repeticao.js
@@ -0,0 +1,11 @@
+let contador = 0
+let valor =100
+let salto = 5
+
+while(contador <= valor){
+    console.log(contador)
+    contador+=salto
+    //contador +=1
+}
+
+console.log("parei de contar")
---
+++ switch.js
@@ -0,0 +1,66 @@
+/*console.log(`Doaçao AACD`)
+
+let codigo = 2
+switch (codigo) {
+    case 1:
+        console.log(`Voce dou 10 Reais`)
+        break
+
+    case 2:
+        console.log(`Voce dou 15 Reais`)
+        break
+
+    case 3:
+        console.log(`Voce dou 25 Reais`)
+        break
+
+    case 4:
+        console.log(`Voce dou 50 Reais`)
+        break
+
+    default:
+        console.log(`Opçao invalida`)
+        break
+} 
+*/
+
+console.log(`DDD`)
+
+let codigo = 80
+switch (codigo) {
+    case 61:
+        console.log(`DDD de Brasilia`)
+        break
+
+    case 71:
+        console.log(`DDD de Salvador`)
+        break
+
+    case 11:
+        console.log(`DDD de São Paulo`)
+        break
+
+    case 21:
+        console.log(`DDD de Rio de Janeiro`)
+        break
+
+    case 32:
+        console.log(`DDD de Juiz de Fora`)
+        break
+
+    case 19:
+        console.log(`DDD de Campinas`)
+        break
+
+    case 27:
+        console.log(`DDD de Vitoria`)
+        break
+
+    case 31:
+        console.log(`DDD de Belo Horizonte`)
+        break
+
+    default: 80
+        console.log(`DDD não cadastado`)
+        break
+}
